a50deb45a0c3e35be2e89ea56090946c720eba59
rizalamar <rizalamarulloh2014@gmail.com>
2025-11-15 01:16:21 +0700

    add function: getToday

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -90,3 +90,11 @@ export function getDate(date) {
 		day: "numeric",
 	});
 }
+
+export function getToday() {
+	const today = new Date();
+	const year = today.getFullYear();
+	const month = today.getMonth() + 1;
+	const day = today.getDate();
+	return `${year}-${month}-${day}`;
+}
