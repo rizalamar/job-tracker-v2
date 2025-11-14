6a30484aecef9e9024d62e2e49eb700c81e226e9
rizalamar <rizalamarulloh2014@gmail.com>
2025-11-14 19:16:28 +0700

    add element in Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
 	return (
 		<Router>
 			<Navbar />
 			<Routes>
-				<Route path="/" element />
+				<Route path="/" element={<Dashboard />} />
 				<Route path="/add" element />
 				<Route path="/job/:id" element />
 				<Route path="/edit/:id" element />
