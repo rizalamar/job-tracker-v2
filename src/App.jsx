9a1881dbf974621dda0ea12144aeacc6ee9b1b82
rizalamar <rizalamarulloh2014@gmail.com>
2025-11-15 01:15:33 +0700

    add element in Route: EditJob.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import AddJob from "./pages/AddJob";
+import EditJob from "./pages/EditJob";
 
 export default function App() {
 	return (
@@ -11,7 +12,7 @@ export default function App() {
 				<Route path="/" element={<Dashboard />} />
 				<Route path="/add" element={<AddJob />} />
 				<Route path="/job/:id" element />
-				<Route path="/edit/:id" element />
+				<Route path="/edit/:id" element={<EditJob />} />
 			</Routes>
 		</Router>
 	);
